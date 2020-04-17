9ab12f1a03a23c0fd742ea099b7cff27cceec797
Rick South <rick@natamus.com>
2020-04-17 15:14:07 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-    location.href = "https://cdn.jsdelivr.net/gh/ricksouth/serilum-forge-maven@master/" + window.location.pathname;
+    location.href = "https://cdn.jsdelivr.net/gh/ricksouth/serilum-forge-maven@master" + window.location.pathname;
 }
